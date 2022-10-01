c604bee549ed2bb38536a78cfc80154acd61aea8
Leonoor's Cryptoman <50612432+LeonoorsCryptoman@users.noreply.github.com>
2022-10-01 15:47:32 +0200

    Add files via upload

--- Chain/vue/src/store/index.ts
+++ Chain/vue/src/store/index.ts
@@ -1,13 +1,27 @@
-import { createStore } from 'vuex'
+// THIS FILE IS GENERATED AUTOMATICALLY. DO NOT MODIFY.
 
-import init from './config'
+import AndromaAndroma from './androma.androma'
 
-const store = createStore({
-  state() {
-    return {}
-  },
-  mutations: {},
-  actions: {}
-})
-init(store)
-export default store
+
+export default { 
+  AndromaAndroma: load(AndromaAndroma, 'androma.androma'),
+  
+}
+
+
+function load(mod, fullns) {
+    return function init(store) {        
+        if (store.hasModule([fullns])) {
+            throw new Error('Duplicate module name detected: '+ fullns)
+        }else{
+            store.registerModule([fullns], mod)
+            store.subscribe((mutation) => {
+                if (mutation.type == 'common/env/INITIALIZE_WS_COMPLETE') {
+                    store.dispatch(fullns+ '/init', null, {
+                        root: true
+                    })
+                }
+            })
+        }
+    }
+}
